293833a56186f433cb6665fe7519f74b2f697ebb
Aditya Rai <adityarai2312@gmail.com>
2026-05-09 12:14:12 +0530

    final-commit

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
